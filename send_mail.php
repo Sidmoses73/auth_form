45d7d45ad635b2a8b5bdcdf53284336ee2c2ba16
Ahonmisi Moses <79580586+Sidmoses73@users.noreply.github.com>
2025-06-27 12:51:51 +0100

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,48 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'mailer/src/PHPMailer.php';
+require 'mailer/src/SMTP.php';
+require 'mailer/src/Exception.php';
+
+function sendVerificationEmail($to, $code) {
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'localhost';
+        $mail->SMTPAuth = false;
+        $mail->Username = '';
+        $mail->Password = '';
+        $mail->SMTPSecure = 'false';
+        $mail->Port = 1025;
+        $mail->setFrom('noreply@example.com', 'Auth form');
+        $mail->addAddress($to);
+        $mail->Subject = 'Your Verification Code';
+        $mail->Body = "Your verification code is: $code";
+        $mail->send();
+    } catch (Exception $e) {
+        die("Email could not be sent. Mailer Error: {$mail->ErrorInfo}");
+    }
+}
+
+function sendPasswordReset($to, $token) {
+    $link = "http://sidmosestech.com/reset_password.php?token=$token";
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'localhost';
+        $mail->SMTPAuth = false;
+        $mail->Username = '';
+        $mail->Password = '';
+        $mail->SMTPSecure = 'false';
+        $mail->Port = 1025;
+        $mail->setFrom('noreply@example.com', 'Auth form');
+        $mail->addAddress($to);
+        $mail->Subject = 'Password Reset';
+        $mail->Body = "Click to reset password: $link";
+        $mail->send();
+    } catch (Exception $e) {
+        die("Email could not be sent. Mailer Error: {$mail->ErrorInfo}");
+    }
+}
+?>
